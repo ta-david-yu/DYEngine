05f5a83fbdfd0ee6c66427e2a448f924144e071b
Ta David Yu <d366095@gmail.com>
2020-12-02 01:00:50 +0100

    Added entry point and define CreateApplication function in TestApp instead

--- DYEngine/include/AppEntryPoint.h
+++ DYEngine/include/AppEntryPoint.h
@@ -1,10 +1,17 @@
-#include "Application.h"
+#pragma once
 
+#include "Application.h"
 // when using SDL, the main function should be like `int main(int argc, char **argv)`
 int main(int argc, char *argv[])
 {
+    auto pApp = DYE::CreateApplication();
+    pApp->Run();
+
+    delete pApp;
+
+    /*
     DYE::Application app { "Main App", 60 };
     app.Run();
-
+    */
     return 0;
 }
