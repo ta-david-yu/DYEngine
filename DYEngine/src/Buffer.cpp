587ef4620391d8918a4c5812581afa1f7033797c
Ta David Yu <d366095@gmail.com>
2021-03-27 17:09:14 +0100

    Bind to ARRAY_BUFFER for index buffer when setting data

--- DYEngine/src/Buffer.cpp
+++ DYEngine/src/Buffer.cpp
@@ -51,8 +51,8 @@ namespace DYE
 
         // GL_ELEMENT_ARRAY_BUFFER is not valid without an actively bound VAO
         // Binding with GL_ARRAY_BUFFER allows the data to be loaded regardless of VAO state.
-        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_ID);
-        glBufferData(GL_ELEMENT_ARRAY_BUFFER, count * sizeof(std::uint32_t), indices, GL_STATIC_DRAW);
+        glBindBuffer(GL_ARRAY_BUFFER, m_ID);
+        glBufferData(GL_ARRAY_BUFFER, count * sizeof(std::uint32_t), indices, GL_STATIC_DRAW);
     }
 
     void IndexBuffer::Bind() const
