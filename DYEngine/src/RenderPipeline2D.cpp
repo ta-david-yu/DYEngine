ef3ab2d84c9b60290a4d0558cfb7f95d127f38a2
Ta David Yu <ta.yu.gamedev@gmail.com>
2022-12-27 16:30:10 +0100

    Added submission sorting before actual render calls

--- DYEngine/src/RenderPipeline2D.cpp
+++ DYEngine/src/RenderPipeline2D.cpp
@@ -12,10 +12,23 @@ namespace DYE
 		for (auto const& camera : cameras)
 		{
 			// Sort the submission (render queue, sorting layer, camera distance etc).
+			// TODO: insertion sort cuz the vector will already be kinda sorted in every Submit call
 			glm::mat4 const viewMatrix = camera.GetViewMatrix();
-			// TODO:
+			std::stable_sort
+			(
+				m_Submissions.begin(),
+				m_Submissions.end(),
+				[viewMatrix](RenderSubmission2D const& submissionA, RenderSubmission2D const& submissionB)
+				{
+					auto modelViewMatrixA = viewMatrix * submissionA.ObjectToWorldMatrix;
+					auto modelViewMatrixB = viewMatrix * submissionB.ObjectToWorldMatrix;
+					glm::vec4 const viewPositionA = modelViewMatrixA * glm::vec4 {0, 0, 0, 1};
+					glm::vec4 const viewPositionB = modelViewMatrixB * glm::vec4 {0, 0, 0, 1};
+					return viewPositionA.z < viewPositionB.z;
+				}
+			);
 
-			// Draw calls.
+			// Execute draw-calls.
 			for (auto const &submission: m_Submissions)
 			{
 				RenderCommand::GetInstance().DrawIndexedNow
@@ -41,6 +54,7 @@ namespace DYE
 			RenderSubmission2D { .VertexArray = vertexArray, .Material = material, .ObjectToWorldMatrix = objectToWorldMatrix }
 		);
 
+		/*
 		Algorithm::InsertionSort
 		(
 			m_Submissions.begin(),
@@ -49,6 +63,6 @@ namespace DYE
 			{
 				return true;
 			}
-		);
+		);*/
 	}
 }
