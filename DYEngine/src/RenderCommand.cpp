bbd7213fe0dea7d6d41d4352111a1190232949d4
Ta David Yu <ta.yu.gamedev@gmail.com>
2022-12-31 00:07:00 +0100

    Fixed resizing window depth buffer issue (enabling DepthMask before glClear works)

--- DYEngine/src/RenderCommand.cpp
+++ DYEngine/src/RenderCommand.cpp
@@ -78,6 +78,8 @@ namespace DYE
 
     void RenderCommand::Clear()
     {
+		// Enable depth mask first so glClear() call actually clears depth buffer.
+		glCall(glDepthMask(GL_TRUE));
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
     }
 
