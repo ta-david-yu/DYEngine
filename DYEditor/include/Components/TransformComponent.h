005dcb29de9c6777968d06839c1e0321c4bf6109
Ta David Yu <ta.yu.gamedev@gmail.com>
2023-05-06 15:29:38 +0200

    Added GetTransformMatrix func in TransformComponent

--- DYEditor/include/Components/TransformComponent.h
+++ DYEditor/include/Components/TransformComponent.h
@@ -10,5 +10,15 @@ namespace DYE::DYEditor
 		glm::vec3 Position {0, 0, 0};
 		glm::vec3 Scale {1, 1, 1};
 		glm::quat Rotation {glm::vec3 {0, 0, 0}};
+
+		glm::mat4 GetTransformMatrix() const
+		{
+			glm::mat4 modelMatrix = glm::mat4 {1.0f};
+			modelMatrix = glm::translate(modelMatrix, Position);
+			modelMatrix = modelMatrix * glm::toMat4(Rotation);
+			modelMatrix = glm::scale(modelMatrix, Scale);
+
+			return modelMatrix;
+		}
 	};
 }
--- DYEditor/src/Render2DSpriteSystem.cpp
+++ DYEditor/src/Render2DSpriteSystem.cpp
@@ -38,11 +38,7 @@ namespace DYE::DYEditor
 
 			Entity wrappedEntity = world.WrapIdentifierIntoEntity(entity);
 
-			glm::mat4 modelMatrix = glm::mat4 {1.0f};
-			modelMatrix = glm::translate(modelMatrix, transform.Position);
-			modelMatrix = modelMatrix * glm::toMat4(transform.Rotation);
-			modelMatrix = glm::scale(modelMatrix, transform.Scale);
-
+			glm::mat4 modelMatrix = transform.GetTransformMatrix();
 
 			// Scale the matrix based on sprite pixels per unit.
 			modelMatrix = glm::scale(modelMatrix, sprite.Texture->GetScaleFromTextureDimensions());
