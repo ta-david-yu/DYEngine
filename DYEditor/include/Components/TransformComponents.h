7e2554dfae87e5560237560a775ef4f249a4f4dd
Ta David Yu <ta.yu.gamedev@gmail.com>
2023-10-27 14:45:56 +0200

    Inlined a couple of LocalToWorld functions

--- DYEditor/include/Components/TransformComponents.h
+++ DYEditor/include/Components/TransformComponents.h
@@ -31,12 +31,12 @@ namespace DYE::DYEditor
 		/// 3D Affine Matrix
 		glm::mat4 Matrix = glm::mat4 {1.0f};
 
-		glm::vec3 GetRight() const { return {Matrix[0][0], Matrix[0][1], Matrix[0][2]}; }
-		glm::vec3 GetUp() const { return {Matrix[1][0], Matrix[1][1], Matrix[1][2]}; }
-		glm::vec3 GetForward() const { return {Matrix[2][0], Matrix[2][1], Matrix[2][2]}; }
+		inline glm::vec3 GetRight() const { return {Matrix[0][0], Matrix[0][1], Matrix[0][2]}; }
+		inline glm::vec3 GetUp() const { return {Matrix[1][0], Matrix[1][1], Matrix[1][2]}; }
+		inline glm::vec3 GetForward() const { return {Matrix[2][0], Matrix[2][1], Matrix[2][2]}; }
 
-		glm::vec3 GetPosition() const { return {Matrix[3][0], Matrix[3][1], Matrix[3][2]}; }
-		glm::quat GetRotation() const
+		inline glm::vec3 GetPosition() const { return {Matrix[3][0], Matrix[3][1], Matrix[3][2]}; }
+		inline glm::quat GetRotation() const
 		{
 			glm::mat3 rotationMatrix = glm::orthonormalize(glm::mat3 {Matrix});
 			return { rotationMatrix };
