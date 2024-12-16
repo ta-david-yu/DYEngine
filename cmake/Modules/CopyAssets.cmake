7e1549e80e60950baee54b7a4151e9829c6d9570
Ta David Yu <ta.yu.gamedev@gmail.com>
2024-12-16 15:48:46 +0100

    Made asset copy command happen in post build and using generate expression, so it's properly copied in multi-config generator (e.g., msvc)

--- cmake/Modules/CopyAssets.cmake
+++ cmake/Modules/CopyAssets.cmake
@@ -51,3 +51,26 @@ function(CopyBinaryAssets ASSETS)
         file(COPY ${ASSET_INPUT_PATH} DESTINATION ${ASSET_OUTPUT_PATH})
     endforeach()
 endfunction()
+
+# Copy all the assets listed in ASSETS as if they are binary files (including text files) in post build stage of the specified target.
+function(CopyAssetsPostBuild BUILD_TARGET ASSETS)
+    # We merge the rest of the arguments with ASSETS to collect all the pass in assets as a list
+    # for more details, see - https://stackoverflow.com/questions/5248749/passing-a-list-to-a-cmake-macro
+    set(ASSETS ${ASSETS} ${ARGN})
+
+    foreach(ASSET ${ASSETS})
+        # set input path (for debugging purpose)
+        set(ASSET_INPUT_PATH ${PROJECT_SOURCE_DIR}/${ASSET})
+
+        # extract filepath
+        get_filename_component(ASSET_PATH ${ASSET} DIRECTORY)
+        get_filename_component(ASSET_NAME ${ASSET} NAME)
+
+        add_custom_command(
+                TARGET ${BUILD_TARGET} POST_BUILD
+                COMMAND ${CMAKE_COMMAND} -E copy
+                ${ASSET_INPUT_PATH}
+                $<TARGET_FILE_DIR:${BUILD_TARGET}>/${ASSET_PATH}/${ASSET_NAME}
+                COMMENT "[${BUILD_TARGET}] Post Build Copy Binary Asset: '${ASSET_INPUT_PATH}' -> '$<TARGET_FILE_DIR:${BUILD_TARGET}>/${ASSET_PATH}'")
+    endforeach()
+endfunction()
